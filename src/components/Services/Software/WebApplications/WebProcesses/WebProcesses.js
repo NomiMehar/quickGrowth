faf002c58e42e02222bf0a77d41bd77cbb8bc8b9
Haseeb_Spaal <119943605+haseebspaal@users.noreply.github.com>
2024-10-29 17:55:36 +0500

    changes Updated

--- src/components/Services/Software/WebApplications/WebProcesses/WebProcesses.js
+++ src/components/Services/Software/WebApplications/WebProcesses/WebProcesses.js
@@ -5,8 +5,8 @@ import AOS from 'aos';
 const WebProcesses = () => {
     useEffect(() => {
         AOS.init();
-      }, [])
-      return (
+    }, [])
+    return (
         <React.Fragment>
             <div className="WebProcesses">
                 <div className="container">
@@ -15,19 +15,43 @@ const WebProcesses = () => {
                     </div>
                     <div className="block">
                         <ul>
-                            <li data-aos="fade-up" data-aos-duration="1000"><img src="/images/services/web-process-1.svg" alt="Web Process Img" /><span>Design</span></li>
-                            <li data-aos="fade-up" data-aos-duration="1000"><img src="/images/services/web-process-2.svg" alt="Web Process Img" /><span>Prototype</span></li>
-                            <li data-aos="fade-up" data-aos-duration="1000"><img src="/images/services/web-process-3.svg" alt="Web Process Img" /><span>Development</span></li>
-                            <li data-aos="fade-up" data-aos-duration="1000"><img src="/images/services/web-process-4.svg" alt="Web Process Img" /><span>Software Quality
-                            Assurance</span></li>
-                            <li data-aos="fade-up" data-aos-duration="1000"><img src="/images/services/web-process-5.svg" alt="Web Process Img" /><span>DevOps Service</span></li>
-                            <li data-aos="fade-up" data-aos-duration="1000"><img src="/images/services/web-process-6.svg" alt="Web Process Img" /><span>Support & Maintenance</span></li>
+                            <li data-aos="fade-up" data-aos-duration="1000">
+                                <img src="/images/services/web-process-1.svg" alt="Web Process Img" />
+                                <p>1</p>
+                                <span>Design</span>
+                            </li>
+                            <li data-aos="fade-up" data-aos-duration="1000">
+                                <img src="/images/services/web-process-2.svg" alt="Web Process Img" />
+                                <p>2</p>
+                                <span>Prototype</span>
+                            </li>
+                            <li data-aos="fade-up" data-aos-duration="1000">
+                                <img src="/images/services/web-process-3.svg" alt="Web Process Img" />
+                                <p>3</p>
+                                <span>Development</span>
+                            </li>
+                            <li data-aos="fade-up" data-aos-duration="1000">
+                                <img src="/images/services/web-process-4.svg" alt="Web Process Img" />
+                                <p>4</p>
+                                <span>Software Quality
+                                    Assurance</span>
+                            </li>
+                            <li data-aos="fade-up" data-aos-duration="1000">
+                                <img src="/images/services/web-process-5.svg" alt="Web Process Img" />
+                                <p>5</p>
+                                <span>DevOps Service</span>
+                            </li>
+                            <li data-aos="fade-up" data-aos-duration="1000">
+                                <img src="/images/services/web-process-6.svg" alt="Web Process Img" />
+                                <p>6</p>
+                                <span>Support & Maintenance</span>
+                            </li>
                         </ul>
                     </div>
                 </div>
             </div>
         </React.Fragment>
-      )
+    )
 }
 
 export default WebProcesses
